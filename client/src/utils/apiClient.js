a78dd459c2ab3072529b1a8803f9e1be99178e14
Yisca Grossman <gyisca@gmail.com>
2024-07-03 00:29:03 +0300

    aadd

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  // baseURL: 'http://localhost:5000/api', 
+  baseURL: 'http://16.170.203.43:5000/api', 
 });
 
 
