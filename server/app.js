3deae7540d64ce25ad35314250940388c2eb22ea
Yisca Grossman <gyisca@gmail.com>
2024-07-03 00:39:16 +0300

    aadds

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
+const path = require('path');
+
 const middleware = require('./utils/middleware');
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/post');
@@ -16,7 +18,7 @@ app.use('/api', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/user', userRoutes);
 
-// app.use(middleware.unknownEndpointHandler);
-// app.use(middleware.errorHandler);
+app.use(express.static(path.resolve(__dirname, '../public/')));
+app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, '../public/index.html')));
 
 module.exports = app;
